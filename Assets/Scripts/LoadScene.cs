e3d502448d8bde46a40f10f50f0fd762d5a362a5
LosSalmonos <example@example.com>
2021-01-02 16:12:42 +0100

    Added 2 players mode
    2 players mode,

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class LoadScene : MonoBehaviour {
+public class LoadScene : MonoBehaviour
+{
 
 	public void LoadHard()
 	{
-		SceneManager.LoadScene ("Hard");
+		SceneManager.LoadScene("Hard");
+	}
+	public void LoadMedium()
+	{
+		SceneManager.LoadScene("Medium");
 	}
 	public void LoadEasy()
 	{
 		SceneManager.LoadScene("Easy");
-    }
+	}
+	public void LoadHardM()
+	{
+		SceneManager.LoadScene("HardM");
+	}
+	public void LoadMediumM()
+	{
+		SceneManager.LoadScene("MediumM");
+	}
+	public void LoadEasyM()
+	{
+		SceneManager.LoadScene("EasyM");
+	}
 	public void LoadMenu()
 	{
 		SceneManager.LoadScene("Menu");
@@ -19,6 +36,14 @@ public class LoadScene : MonoBehaviour {
 	public void LoadOther()
 	{
 		SceneManager.LoadScene("Other");
+	}
+	public void LoadMulti()
+	{
+		SceneManager.LoadScene("Multiplayer");
+	}
+	public void LoadSing()
+	{
+		SceneManager.LoadScene("Singleplayer");
 	}
 
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,8 @@ public class PlayerMovement	 : MonoBehaviour
 
     public GameObject character;
 	public Rigidbody rb;
+	public bool Player1 = true;
+	public bool Touch = false;
 
 	// Use this for initialization
 	void Start()
@@ -29,45 +31,71 @@ public class PlayerMovement	 : MonoBehaviour
 	{
 		//Debug.Log(Input.GetAxis("Horizontal"));
 		int i = 0;
-		//loop over every touch found
-		while (i < Input.touchCount)
-		{
-			float v = ScreenWidth - (ScreenWidth / 6);
-			if (Input.GetTouch(i).position.x > v)
+        //loop over every touch found
+        if (Touch)
+        {
+
+			while (i < Input.touchCount)
 			{
-				if (Input.GetTouch(i).position.y < ScreenHeight / 3.50)
+				float v = ScreenWidth - (ScreenWidth / 6);
+				if (Input.GetTouch(i).position.x > v)
 				{
-					//move right
-					RunCharacter(1.0f);
+					if (Input.GetTouch(i).position.y < ScreenHeight / 3.50)
+					{
+						//move right
+						RunCharacter(1.0f);
+					}
+
 				}
-				
-			}
-			if (Input.GetTouch(i).position.x < ScreenWidth / 6)
-			{
-				if (Input.GetTouch(i).position.y < ScreenHeight / 3.50)
+				if (Input.GetTouch(i).position.x < ScreenWidth / 6)
 				{
-					//move left
-					RunCharacter(-1.0f);
-				}               
+					if (Input.GetTouch(i).position.y < ScreenHeight / 3.50)
+					{
+						//move left
+						RunCharacter(-1.0f);
+					}
+				}
+				++i;
 			}
-			++i;
+
 		}
+		characterBody.AddForce(0, 0, moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
 	}
 	void FixedUpdate()
 	{
-		characterBody.AddForce(0, 0, moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
-	
-		RunCharacter(Input.GetAxis("Vertical"));
+		
 
-		if (Input.GetKey("d"))
+		if(Touch)
 		{
-			rb.AddForce(sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			RunCharacter(Input.GetAxis("Vertical"));
+		}
+		
+
+		if(Player1)
+		{
+			if (Input.GetKey("d"))
+			{
+				rb.AddForce(sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			}
+
+			if (Input.GetKey("a"))
+			{
+				rb.AddForce(-sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			}
+		}
+		else
+		{
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				rb.AddForce(sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			}
+
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				rb.AddForce(-sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			}
 		}
 
-		if (Input.GetKey("a"))
-		{
-			rb.AddForce(-sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-		}
 	}
 
 	private void RunCharacter(float horizontalInput)
